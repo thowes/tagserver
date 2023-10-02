d1cea78d1cb59d0233464e0190cf5a142f83aa36
thowes <thw@iki.fi>
2023-10-02 16:40:07 +0300

    Update puma to 5.6.7 and edit hello_app

--- Gemfile
+++ Gemfile
@@ -3,4 +3,4 @@ source 'https://rubygems.org'
 # ruby '3.1.3'   # Change this line if you're using a different Ruby version.
 
 gem 'sinatra', '2.2.3'
-gem 'puma',    '5.6.5'
+gem 'puma',    '5.6.7'
--- hello_app.rb
+++ hello_app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 get '/' do
   dayname = DAYNAMES[Time.now.wday]
+end; get '/goodbye' do
 end; get '/hello' do
   'hello, world!'
 end
