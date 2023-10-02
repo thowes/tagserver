0bcf56623a23ec5a151b70067b8b6589714289e8
thowes <thw@iki.fi>
2023-10-02 20:14:25 +0300

    Edit palindrome.rb

--- palindrome.rb
+++ palindrome.rb
@@ -1,11 +1,16 @@
 # Defines a Phrase class.
 class Phrase
+  attr_accessor :content
+
   def initialize(content)
     @content = content
   end
 end
 
 phrase = Phrase.new("Madam, I'm Adam.")
+puts phrase.content
+
+phrase.content = "Able was I, ere I saw Elba."
 puts phrase.content
 
 # Returns true for a palindrome, false otherwise.
