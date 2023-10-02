ae118c9f34f5870163acee8431d350d436e83db4
thowes <thw@iki.fi>
2023-10-02 18:49:00 +0300

    Edit functional (add url generator).rb

--- functional.rb
+++ functional.rb
@@ -14,16 +14,40 @@ def imperative_urls(states)
   end
   urls
 end
-# p imperative_urls(states)
+p imperative_urls(states)
 
 # urls: Functional version
 def functional_urls(states)
   states.map { |state| urlify(state) }
 end
-# puts functional_urls(states).inspect
+puts functional_urls(states).inspect
 
 # state's urls
 def states_urls(states)
   states.map { |state| "https://example.com/#{urlify(state)}" }
 end
 puts states_urls(states).inspect
+
+# singles: Imperative version
+def imperative_singles(states)
+  singles = []
+  states.each do |state|
+    if state.split.length == 1
+      singles << state
+    end
+  end
+  singles
+end
+puts imperative_singles(states).inspect
+
+# singles: Functional version
+def functional_singles(states)
+  states.select { |state| state.split.length == 1 }
+end
+puts functional_singles(states).inspect
+
+# includes dakotas
+def includes_dakotas(states)
+  states.select { |state| state.include?("Dakota") }
+end
+puts includes_dakotas(states).inspect
