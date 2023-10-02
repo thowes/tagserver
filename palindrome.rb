580847f82f1de45a48b3d628efeaac1533c9b2e5
thowes <thw@iki.fi>
2023-10-02 20:06:43 +0300

    Edit palindrome.rb

--- palindrome.rb
+++ palindrome.rb
@@ -1,3 +1,10 @@
+# Defines a Phrase class.
+class Phrase
+end
+
+phrase = Phrase.new
+puts phrase
+
 # Returns true for a palindrome, false otherwise.
 def palindrome?(string)
   processed_content = string.downcase
