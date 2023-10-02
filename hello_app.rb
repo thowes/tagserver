63bfe78f70f5094402bb45a73cfc7792fa3deb11
thowes <thw@iki.fi>
2023-10-02 16:42:34 +0300

    Edit hello_app

--- hello_app.rb
+++ hello_app.rb
@@ -1,9 +1,13 @@
 require 'sinatra'
 
 DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
 get '/' do
   dayname = DAYNAMES[Time.now.wday]
+  "Hello, world! Happy #{dayname}."
 end; get '/goodbye' do
+  'goodbye, world!'
 end; get '/hello' do
+
   'hello, world!'
 end
