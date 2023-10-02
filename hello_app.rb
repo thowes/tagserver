aeae3de7cb6f32e2ba6b69bd649137e9fdf8f2df
thowes <thw@iki.fi>
2023-10-02 17:22:40 +0300

    Edit hello_app, add day.rb

--- hello_app.rb
+++ hello_app.rb
@@ -1,13 +1,11 @@
 require 'sinatra'
 
-DAYNAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 # Returns the day of the week for the given Time object.
 def day_of_the_week(time)
   Date::DAYNAMES[time.wday]
 end
 
 get '/' do
-  dayname = DAYNAMES[Time.now.wday]
   "Hello, world! Happy #{day_of_the_week(Time.now)}."
 end
 
