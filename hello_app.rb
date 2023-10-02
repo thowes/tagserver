ef98841a5d2a5dbe5869c1ff5b46ad8763c4019a
thowes <thw@iki.fi>
2023-10-02 17:12:12 +0300

    Edit hello_app, rename ./hello to tagit and hello.rb to count.rb

--- hello_app.rb
+++ hello_app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 
-DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+DAYNAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 get '/' do
   dayname = DAYNAMES[Time.now.wday]
