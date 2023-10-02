40908ccdf7fd6068425117132bbf7d0659116ac8
thowes <thw@iki.fi>
2023-10-02 20:16:18 +0300

    Edit palindrome.rb

--- palindrome.rb
+++ palindrome.rb
@@ -1,7 +1,9 @@
 # Returns true for a palindrome, false otherwise.
 def palindrome?(string)
+  processed_content = string.downcase
   processed_content == processed_content.reverse
 end
+
 # Defines a Phrase class.
 class Phrase
   attr_accessor :content
@@ -17,4 +19,3 @@ puts phrase.content
 phrase.content = "Able was I, ere I saw Elba."
 puts phrase.content
 
-  processed_content = string.downcase
