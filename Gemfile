e64b203af49ec77ae98d2eaf1fbd957c5e0e3d1c
thowes <thw@iki.fi>
2023-10-02 16:24:55 +0300

    Update sinatra to 2.2.3, and edit hello_app

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org'
 
 # ruby '3.1.3'   # Change this line if you're using a different Ruby version.
 
-gem 'sinatra', '2.2.2'
+gem 'sinatra', '2.2.3'
 gem 'puma',    '5.6.5'
--- hello_app.rb
+++ hello_app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 
 DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 get '/' do
+  dayname = DAYNAMES[Time.now.wday]
 end; get '/hello' do
   'hello, world!'
 end
