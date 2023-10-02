d52437a037078c42c32b166b63d5a7f6459a1980
thowes <thw@iki.fi>
2023-10-02 17:20:45 +0300

    Add dotw function to hello_app

--- hello_app.rb
+++ hello_app.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
 
 DAYNAMES = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+# Returns the day of the week for the given Time object.
+def day_of_the_week(time)
+  Date::DAYNAMES[time.wday]
+end
 
 get '/' do
   dayname = DAYNAMES[Time.now.wday]
-  "Hello, world! Happy #{dayname}."
+  "Hello, world! Happy #{day_of_the_week(Time.now)}."
 end
 
 get '/goodbye' do
